f363738dfcfb8bdb057882c78c9cdc26e7c0a020
Ali <aliwhitrow1@gmail.com>
2026-01-28 21:59:32 +0000

    font working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import Nav from "./components/Nav.jsx";
-import css from "./App.css";
+import "./App.css";
 
 export default function App(){
 
